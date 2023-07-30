744b8b1cf2fff3f2efa37edf21a4163f003da74b
Mostafa Rostam Dokht Effati <mostafa.rostamdokht@gmail.com>
2023-07-30 11:19:07 +0330

    chore: Updating paginated model

--- lib/paginated.model.ts
+++ lib/paginated.model.ts
@@ -2,10 +2,4 @@ export class PaginatedModel<T>
 {
     total: number
     list: T[]
-
-    constructor(options: { total: number, list: T[] })
-    {
-        this.total = options.total
-        this.list = options.list
-    }
 }
